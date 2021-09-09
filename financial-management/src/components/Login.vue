25127b14df050d05680fa7bd7bd9e6911d70d5f8
fernandocm121 <fernandocm121@gmail.com>
2021-09-09 01:39:45 -0300

    feat(components): add login component

---
+++ financial-management/src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full flex justify-center">
+    <div 
+      class="w-1/3 bg-white"
+    >
+      Realize o login
+      <div class="mx-4 my-4">
+        <input
+          class="px-4 w-full border-2 py-2 rounded-md text-sm outline-none"
+          placeholder="email"
+          type="email" 
+        >
+        <input
+          class="px-4 w-full border-2 py-2 rounded-md text-sm outline-none"
+          placeholder="password"
+          type="password"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
